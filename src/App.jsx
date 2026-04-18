d4dd2042c15821bf870f5aebdd442b23b65c5bf9
singhpunyadeep <singhpunyadeep@gmail.com>
2026-04-19 02:01:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-\import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { supabase } from "./AuthWrapper.jsx";
 
 const T = {
